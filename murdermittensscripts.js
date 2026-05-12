e10ae0ff4b88204fe95b8788bc998c664939323c
cpcckl <klor0007@email.cpcc.edu>
2026-05-12 19:47:32 -0400

    Update murdermittensscripts.js

--- murdermittensscripts.js
+++ murdermittensscripts.js
@@ -12,8 +12,7 @@ var x = setInterval(function() {
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	document.getElementbyID("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+	document.getElementbyID("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 	if (distance < 0) {
 		clearInterval(x);
 		document.getElementById("countdown").innerHTML = "Website shut down.";
